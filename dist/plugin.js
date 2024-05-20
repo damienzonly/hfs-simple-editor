cb40c27f405e80e0c488fcb28b7858b03fe18d82
damienzonly <damianomails@gmail.com>
2024-05-20 18:37:15 +0200

    escaping html, preview, editing title #1

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,8 +1,9 @@
-exports.version = 1.2
+exports.version = 1.3
 exports.description = "Simple text editor"
 exports.apiRequired = 8.65
 exports.repo = "damienzonly/hfs-simple-editor"
 exports.frontend_js = ['editor.js', 'main.js']
+exports.preview = ["https://github.com/damienzonly/hfs-simple-editor/assets/38798780/a05529b6-2c88-4515-a21e-ec549466bc26", "https://github.com/damienzonly/hfs-simple-editor/assets/38798780/8dd24d16-5bd8-4796-a4af-f69b021ef75c", "https://github.com/damienzonly/hfs-simple-editor/assets/38798780/2d009f48-74ad-49a0-8d2a-1136839d9cd1", "https://github.com/damienzonly/hfs-simple-editor/assets/38798780/8c386e27-5726-4219-a6f5-27c8371da694"]
 
 exports.config = {
     limitMB: {frontend: true, type: 'number', label: 'Limit MB', helperText: 'Disable edit on files greater than this limit', defaultValue: 1, min: 0},
--- dist/public/main.js
+++ dist/public/main.js
@@ -1,19 +1,29 @@
 {
+    function escapeHtml(html) {
+        return html
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#039;');
+    }
     const limit = HFS.getPluginConfig()['limitMB'] * Math.pow(1024, 2)
     const r = HFS.React
     HFS.onEvent('fileMenu', ({ entry: ent }) => {
         const showCreateFile = ent.isFolder && ent.uri === location.pathname && HFS.state.props.can_upload
         if ((ent.isFolder && !showCreateFile) || !HFS.state.props.can_upload || ent?.s > limit) return
-        const props = { 
-            div: { style: { marginTop: 10, display: 'flex', justifyContent: 'flex-end' }},
-            label: { style: { marginBottom: 10, display: 'block' }},
+        const props = {
+            div: { style: { marginTop: 10, display: 'flex', justifyContent: 'flex-end' } },
+            label: { style: { marginBottom: 10, display: 'block' } },
             input: sf => ({ name: 'input', onChange(e) { sf(e.target.value) } }),
             button: (f, d) => ({ onClick: () => createFile(ent.uri, f, d) }),
             // support enter to submit
-            form: (f, d) => ({ onSubmit: (e) => {
-                e.preventDefault()
-                createFile(ent.uri, f, d)
-            } })
+            form: (f, d) => ({
+                onSubmit: (e) => {
+                    e.preventDefault()
+                    createFile(ent.uri, f, d)
+                }
+            })
         }
         let dialog
         const dialogDescriptor = showCreateFile ? {
@@ -31,16 +41,17 @@
                 )
             }
         } : {
+            title: `Editing ${ent.name}`,
             Content() {
                 const [value, change] = r.useState('')
                 r.useEffect(() => {
-                    fetch(ent.uri + '?dl', {cache: 'no-store'}).then(v => v.text()).then(change).catch(() => change(''))
+                    fetch(ent.uri + '?dl', { cache: 'no-store' }).then(v => v.text()).then(change).catch(() => change(''))
                 }, [])
                 return HFS.h(r.Fragment, null, HFS.h(Editor, {
                     value,
                     onValueChange: change,
                     style: { minWidth: '40vw', minHeight: '30vh', resize: 'both' },
-                    highlight: () => value,
+                    highlight: () => escapeHtml(value),
                 }), HFS.h('button', { style: { marginTop: 20 }, onClick: save }, 'Save'))
 
                 function save() {
@@ -61,13 +72,13 @@
         if (!filename) return
         const uri = `${path}/${filename}`
         // check if filename already exists, with fresh data instead of HFS.state.list
-        const s = await HFS.apiCall('get_file_list', {uri: path})
+        const s = await HFS.apiCall('get_file_list', { uri: path })
         if (s.list.find(f => !f.isFolder && f.n === filename))
-        if (s)
-            return HFS.dialogLib.newDialog({
-                title: 'Conflict',
-                Content: () => HFS.h('div', null, 'File already exists, choose a different name')
-            })
-        return fetch(uri, { method: 'PUT', body: ''}).then(d.close).then(HFS.reloadList)
+            if (s)
+                return HFS.dialogLib.newDialog({
+                    title: 'Conflict',
+                    Content: () => HFS.h('div', null, 'File already exists, choose a different name')
+                })
+        return fetch(uri, { method: 'PUT', body: '' }).then(d.close).then(HFS.reloadList)
     }
 }
